ca4bf4b870fb9144709612aece4bea34b31c5d9d
Andreas Poulsen <bpoudev@gmail.com>
2016-01-19 13:19:47 +0100

    Et commit inden jeg reworker config-filen

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var jade = require("jade");
 
 var aktier = {
     "smør": 100,
-    "mælk": 120
+    "mælk": 10
 };
+
+var teams = {};
+var startMoney = 600;
 
 server = http.createServer(function(req, res){
     if (req.url.indexOf("team") > 0) {
@@ -22,10 +25,49 @@ console.log("Server running on port 8080");
 
 var io = require("socket.io")(server);
 io.on("connection", function(socket) {
-    console.log("Connection");
-/*    setInterval(function() {
-        io.emit("update_aktier", JSON.stringify(aktier));
-    }, 500);*/  
+    var team;
+    socket.on("sign_in", function(msg) {
+        if (! teams.hasOwnProperty(msg)) {
+            teams[msg] = {'aktier': {}, money: startMoney};
+            for (key in aktier) {
+                teams[msg]['aktier'][key] = 0;
+            }
+        }
+        team = msg;
+        
+        function sendOwnDetails() {
+            socket.emit("own_details", teams[team]);
+        }
+        
+        //Initialize all possible actions. Shouldn't be avaliable before sign-in
+        socket.on("buy_action", function(msg) { //msg=["aktie", amount]
+            var aktie = msg[0];
+            var amount = parseInt(msg[1]);
+            teams[team]['money'] -= aktier[aktie] * amount;
+            teams[team]['aktier'][aktie] += amount;
+            
+            //Update stock value
+            aktier[aktie] *= 1 + (Math.log(amount) / Math.log(10))
+            aktier[aktie] = Math.round(aktier[aktie]);
+            
+            sendOwnDetails();
+        });
+        socket.on("sell_action", function(msg) { //msg=["aktie", amount]
+            var aktie = msg[0];
+            var amount = parseInt(msg[1]);
+            var prevAmount = teams[team]['aktier'][aktie];
+            amount = Math.min(amount, prevAmount);
+            teams[team]['money'] += amount * aktier[aktie];
+            teams[team]['aktier'][aktie] -= amount;
+            sendOwnDetails();
+        });
+        socket.on("card_action", function(msg) {
+            
+        });
+        
+        //Send the team its own details
+        sendOwnDetails();
+    });
 });
 
 function updateAktier() {
@@ -36,8 +78,11 @@ function updateAktier() {
         var high = trend + (error/2);
         var low = trend - (error/2);
         aktier[key] += Math.floor(Math.random() * (high - low + 1) + low); // Generate a random new value in the range {trend +- (error/2)}
+        if (aktier[key] < 0) {
+            aktier[key] += Math.abs(Math.floor(aktier[key]/10))*(trend*-2);
+        }
     }
-    io.emit("update_aktier", JSON.stringify(aktier));
+    io.emit("update_aktier", aktier);
 }
 
 setInterval(updateAktier, 1000);
